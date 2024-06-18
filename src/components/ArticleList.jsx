3caaaf1487506517b1bad52e5ddcc96d8595ae1f
Danielle Dawson <danielle.daisy.97@gmail.com>
2024-06-18 04:02:21 +0100

    creates SortControls component to sort the list of articles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 import { getArticles } from "../api";
 import ArticleCard from "./ArticleCard";
 import TopicDropdown from "./TopicDropdown";
+import SortControls from "./SortControls";
 import "../components-css/ArticleList.css";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [error, setError] = useState(null);
   const [selectedTopic, setSelectedTopic] = useState("");
+  const [sortBy, setSortBy] = useState("created_at");
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
-    getArticles(selectedTopic)
+    getArticles(selectedTopic, sortBy, order)
       .then((articles) => {
         setArticles(articles);
       })
@@ -18,15 +21,21 @@ const ArticleList = () => {
         console.error("Error fetching articles:", error);
         setError("Failed to fetch articles. Please try again later.");
       });
-  }, [selectedTopic]);
+  }, [selectedTopic, sortBy, order]);
 
   return (
     <div className="article-list">
       <TopicDropdown onSelectTopic={setSelectedTopic} />
+      <SortControls
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        order={order}
+        setOrder={setOrder}
+      />
       {error && <p className="error">{error}</p>}
       {articles.length > 0
         ? articles.map((article) => (
-            <ArticleCard key={article.title} article={article} />
+            <ArticleCard key={article.article_id} article={article} />
           ))
         : !error && <p>Loading articles...</p>}
     </div>
